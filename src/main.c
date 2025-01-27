b0392b6c289556869bb4dce5b8ed594df5d2b5f0
0xFA99 <fajar.nur.h@pm.me>
2025-01-27 14:53:49 +0700

    add camera

--- src/main.c
+++ src/main.c
@@ -28,6 +28,12 @@ int main(void)
 
     SetPlayerAnimation(&player, STATE_IDLE, DIRECTION_RIGHT);
 
+    Camera2D camera = { 0 };
+    camera.offset = (Vector2){ GetScreenWidth()/2.0f, GetScreenHeight()/2.0f };
+    camera.target = (Vector2){ player.movement.position.x, player.movement.position.y };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
@@ -38,9 +44,16 @@ int main(void)
 
         UpdatePlayer(&player, GetFrameTime());
 
+        if (IsKeyDown(KEY_Z)) camera.zoom += 0.05f;
+        if (IsKeyDown(KEY_X)) camera.zoom -= 0.05f;
+
+        camera.target.x = player.movement.position.x + player.entity->sheetData.frameRects[0].width/2.0f;
+
         BeginDrawing();
             ClearBackground((Color){ 18, 18, 18, 255 });
-            DrawPlayer(&player);
+            BeginMode2D(camera);
+                DrawPlayer(&player);
+            EndMode2D();
         EndDrawing();
     }
 
